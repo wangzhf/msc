78038b9c042dbe4e5b91bcd772ab433977ad1df8
wangzhifeng <wangzhifeng@aiops.com>
2020-06-16 16:13:24 +0800

    add spring cloud gateway

---
+++ gateway/scg/src/main/java/com/wangzhf/msc/scg/SCGApp.java
@@ -0,0 +1,24 @@
+package com.wangzhf.msc.scg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SCGApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCGApp.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/about")
+                                        .uri("http://www.baidu.com")
+                ).build();
+    }
+
+}
